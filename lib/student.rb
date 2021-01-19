f2f828d23dd1a732a6f2afdf899f6eec65e81d65
Marc Steele <marcsteele2014@gmail.com>
2021-01-19 19:28:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,10 @@ class Student
   end
 
   def self.students_below_12th_grade
-
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade < 12
+    SQL
+    DB[:conn].execute(sql).map { |row| self.new_from_db(row) }
   end
 
   def save
